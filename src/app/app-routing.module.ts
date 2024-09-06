27ea321431cf3c968af1e57475a4a727d5b0454c
Wasiq Sheikh <wasiqsheikh1998@gmail.com>
2024-09-07 01:34:28 +0500

    Complete

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,10 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'song',
+    loadChildren: () => import('./song/song.module').then( m => m.SongPageModule)
+  },
 ];
 
 @NgModule({
